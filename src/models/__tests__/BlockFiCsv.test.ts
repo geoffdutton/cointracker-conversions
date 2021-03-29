81db3239830b910011b770bba5f31c46ca5b4fb4
Geoff Dutton <g.dutton@gmail.com>
2021-03-28 20:17:17 -0500

    refactor mock streams

--- src/models/__tests__/BlockFiCsv.test.ts
+++ src/models/__tests__/BlockFiCsv.test.ts
@@ -1,73 +1,33 @@
-import fs from 'fs'
-import { PassThrough } from 'stream'
+import fixtures, { FixtureMockStreams } from './fixtures/fixtures'
 import { BlockFiCsv } from '../BlockFiCsv'
 import path from 'path'
 import { OUTPUT_DIR } from '../../constants'
+const sourceCsv = fixtures.loadFixture('BlockFi')
 
-interface MockFs {
-  createReadStream: jest.Mock<PassThrough>
-  createWriteStream: jest.Mock<PassThrough>
-}
-
-jest.mock(
-  'fs',
-  (): MockFs => {
-    return {
-      createReadStream: jest.fn(() => new PassThrough()),
-      createWriteStream: jest.fn(() => new PassThrough())
-    }
-  }
-)
-
-describe('blockchain.com', () => {
+describe('BlockFi', () => {
   const sourceFile = 'some/source/file.csv'
   const timezone = 'America/Chicago'
   let testee: BlockFiCsv
 
-  const sourceCsv = `Cryptocurrency,Amount,Transaction Type,Confirmed At
-LINK,60.51863375,Deposit,2021-03-04 13:27:35
-ETH,0.06321840,Interest Payment,2021-02-28 23:59:59
-USDC,136.62968829,Interest Payment,2021-02-28 23:59:59
-USDC,-0.25000000,Withdrawal Fee,2021-02-25 19:38:10
-USDC,-4499.75000000,Withdrawal,2021-02-25 19:38:10`
-
-  let mockFsCreateReadStream: jest.Mock
-  let mockFsCreateWriteStream: jest.Mock
-  let chunksWritten: string[]
+  let mocks: FixtureMockStreams
 
   beforeEach(() => {
-    chunksWritten = []
-    mockFsCreateReadStream = fs.createReadStream as jest.Mock
-    mockFsCreateReadStream.mockImplementationOnce(() => {
-      const stream = new PassThrough()
-      stream.push(Buffer.from(sourceCsv))
-      stream.push(null)
-      return stream
-    })
-
-    const writeStream = new PassThrough()
-    mockFsCreateWriteStream = fs.createWriteStream as jest.Mock
-    mockFsCreateWriteStream.mockImplementationOnce(() => {
-      writeStream.write = jest.fn((chunk?, cb?) => {
-        chunksWritten.push(Buffer.from(chunk).toString('utf8').trim())
-        cb && cb(null)
-      }) as jest.Mock
-      return writeStream
-    })
+    mocks = fixtures.setupMockStreams(sourceCsv)
+    testee = new BlockFiCsv(sourceFile, timezone)
   })
 
-  beforeEach(() => {
-    testee = new BlockFiCsv(sourceFile, timezone)
+  afterEach(() => {
+    mocks.restore()
   })
 
   test('success', async () => {
     await testee.convert()
-    expect(mockFsCreateReadStream).toHaveBeenCalledWith(sourceFile)
-    expect(mockFsCreateWriteStream).toHaveBeenCalledWith(
+    expect(mocks.mockFsCreateReadStream).toHaveBeenCalledWith(sourceFile)
+    expect(mocks.mockFsCreateWriteStream).toHaveBeenCalledWith(
       path.resolve(OUTPUT_DIR, 'file_contrackered.csv')
     )
-    expect(chunksWritten.length).toBe(5)
-    expect(chunksWritten).toMatchInlineSnapshot(`
+    expect(mocks.chunksWritten.length).toBe(5)
+    expect(mocks.chunksWritten).toMatchInlineSnapshot(`
       Array [
         "Date,Received Quantity,Received Currency,Sent Quantity,Sent Currency,Fee Amount,Fee Currency,Tag",
         "03/04/2021 07:27:35,60.51863375,LINK,,,,,",
--- src/models/__tests__/BlockchainDotComCsv.test.ts
+++ src/models/__tests__/BlockchainDotComCsv.test.ts
@@ -1,5 +1,4 @@
-import fs from 'fs'
-import { PassThrough } from 'stream'
+import fixtures, { FixtureMockStreams } from './fixtures/fixtures'
 import {
   BlockchainDotComCsv,
   BlockchainDotComCsvRow
@@ -9,25 +8,11 @@ import { OUTPUT_DIR } from '../../constants'
 import path from 'path'
 import createConvertibleCsv from '../ConvertibleCsv'
 
-interface MockFs {
-  createReadStream: jest.Mock<PassThrough>
-  createWriteStream: jest.Mock<PassThrough>
-}
-
-jest.mock(
-  'fs',
-  (): MockFs => {
-    return {
-      createReadStream: jest.fn(() => new PassThrough()),
-      createWriteStream: jest.fn(() => new PassThrough())
-    }
-  }
-)
-
 describe('blockchain.com', () => {
   const sourceFile = 'some/source/file.csv'
   const timezone = 'America/Chicago'
   let testee: BlockchainDotComCsv
+
   beforeEach(() => {
     testee = new BlockchainDotComCsv(sourceFile, timezone)
   })
@@ -94,39 +79,16 @@ describe('blockchain.com', () => {
 
   describe('convert()', () => {
     const sourceFileError = 'some/file/bad.csv'
-    const sourceCsv = `"date","time","token","type","amount","value_then","value_now","exchange_rate_then","tx","note"
-"2020-10-17","15:34:56 GMT +00:00","BTC","sent","-0.03897608","$ -442.50","$ -2,179.89","$ 11,353.18","ajifoeji",""
-"2020-10-17","14:25:37 GMT +00:00","BTC","received","0.01385318","$ 157.10","$ 774.79","$ 11,340.00","aiojfiowe",""
-"2020-10-17","14:25:37 GMT +00:00","BTC","received","0.01770809","$ 200.81","$ 990.40","$ 11,340.00","aifjoiew",""
-"2020-10-14","04:25:04 GMT +00:00","BTC","received","0.00741481","$ 84.67","$ 414.70","$ 11,418.95","aifojew",""`
+    const sourceCsv = fixtures.loadFixture('blockchain.com')
 
-    let mockFsCreateReadStream: jest.Mock
-    let mockFsCreateWriteStream: jest.Mock
-    let chunksWritten: string[]
+    let mocks: FixtureMockStreams
 
     beforeEach(() => {
-      chunksWritten = []
-      mockFsCreateReadStream = fs.createReadStream as jest.Mock
-      mockFsCreateReadStream.mockImplementationOnce((fileName: string) => {
-        const stream = new PassThrough()
-        stream.push(Buffer.from(sourceCsv))
-        if (fileName === sourceFileError) {
-          // Unexpected Error: column header mismatch
-          stream.push(Buffer.from(sourceCsv))
-        }
-        stream.push(null)
-        return stream
-      })
+      mocks = fixtures.setupMockStreams(sourceCsv, sourceFileError)
+    })
 
-      const writeStream = new PassThrough()
-      mockFsCreateWriteStream = fs.createWriteStream as jest.Mock
-      mockFsCreateWriteStream.mockImplementationOnce(() => {
-        writeStream.write = jest.fn((chunk?, cb?) => {
-          chunksWritten.push(Buffer.from(chunk).toString('utf8').trim())
-          cb && cb(null)
-        }) as jest.Mock
-        return writeStream
-      })
+    afterEach(() => {
+      mocks.restore()
     })
 
     test('success', async () => {
@@ -137,12 +99,12 @@ describe('blockchain.com', () => {
       )
 
       await testee.convert()
-      expect(mockFsCreateReadStream).toHaveBeenCalledWith(sourceFile)
-      expect(mockFsCreateWriteStream).toHaveBeenCalledWith(
+      expect(mocks.mockFsCreateReadStream).toHaveBeenCalledWith(sourceFile)
+      expect(mocks.mockFsCreateWriteStream).toHaveBeenCalledWith(
         path.resolve(OUTPUT_DIR, 'file_contrackered.csv')
       )
-      expect(chunksWritten.length).toBe(5)
-      expect(chunksWritten[0]).toEqual(
+      expect(mocks.chunksWritten.length).toBe(5)
+      expect(mocks.chunksWritten[0]).toEqual(
         [
           'Date',
           'Received Quantity',
@@ -154,7 +116,7 @@ describe('blockchain.com', () => {
           'Tag'
         ].join(',')
       )
-      expect(chunksWritten).toMatchInlineSnapshot(`
+      expect(mocks.chunksWritten).toMatchInlineSnapshot(`
       Array [
         "Date,Received Quantity,Received Currency,Sent Quantity,Sent Currency,Fee Amount,Fee Currency,Tag",
         "10/17/2020 10:34:56,,,0.03897608,BTC,,,",
@@ -167,7 +129,6 @@ describe('blockchain.com', () => {
 
     test('csv error', async () => {
       testee = new BlockchainDotComCsv(sourceFileError, timezone)
-
       await expect(testee.convert()).rejects.toThrowError(
         'Unexpected Error: column header mismatch expected: 10 columns got: 19'
       )
